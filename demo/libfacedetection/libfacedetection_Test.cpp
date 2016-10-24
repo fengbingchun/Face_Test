06b4ec8dcca05f49d06d43673a24ba519f0b42c8
fengbingchun <fengbingchun@163.com>
2016-10-24 21:35:48 +0800

    Add libfacedetection test project

---
+++ demo/libfacedetection/libfacedetection_Test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <facedetect-dll.h>
+
+int main()
+{
+
+	std::cout << "ok" << std::endl;
+	return 0;
+}
--- src/libfacedetection/include/facedetect-dll.h
+++ src/libfacedetection/include/facedetect-dll.h
@@ -27,40 +27,40 @@ SOFTWARE.
 #define FACEDETECT_DLL_H
 
 #ifdef FACEDETECTDLL_EXPORTS
-#define FACEDETECTDLL_API __declspec(dllexport) 
+#define FACEDETECTDLL_API __declspec(dllexport)
 #else
 #ifdef __linux__
-#define FACEDETECTDLL_API  
+#define FACEDETECTDLL_API
 #else
 #define FACEDETECTDLL_API __declspec(dllimport) 
 #endif
 #endif
 
-FACEDETECTDLL_API int * facedetect_frontal( unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
-                               float scale, //scale factor for scan windows
-                               int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
-                               int min_object_width, //Minimum possible face size. Faces smaller than that are ignored.
-							   int max_object_width=0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
+FACEDETECTDLL_API int * facedetect_frontal(unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
+	float scale, //scale factor for scan windows
+	int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
+	int min_object_width, //Minimum possible face size. Faces smaller than that are ignored.
+	int max_object_width = 0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
 
-FACEDETECTDLL_API int * facedetect_multiview( unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
-                               float scale, //scale factor for scan windows
-                               int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
-                               int min_object_width, //Minimum possible face size. Faces smaller than that are ignored.
-							   int max_object_width=0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
+FACEDETECTDLL_API int * facedetect_multiview(unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
+	float scale, //scale factor for scan windows
+	int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
+	int min_object_width, //Minimum possible face size. Faces smaller than that are ignored.
+	int max_object_width = 0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
 
-FACEDETECTDLL_API int * facedetect_multiview_reinforce( unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
-                               float scale, //scale factor for scan windows
-                               int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
-                               int min_object_width, //Minimum possible face size. Faces smaller than that are ignored.
-							   int max_object_width=0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
+FACEDETECTDLL_API int * facedetect_multiview_reinforce(unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
+	float scale, //scale factor for scan windows
+	int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
+	int min_object_width, //Minimum possible face size. Faces smaller than that are ignored.
+	int max_object_width = 0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
 
 /*
  This function is designed for images in video surveillance.
  */
-FACEDETECTDLL_API int * facedetect_frontal_surveillance( unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
-                               float scale, //scale factor for scan windows
-                               int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
-                               int min_object_width, //Minimum possible face size. Faces smaller than that are ignored.
-							   int max_object_width=0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
+FACEDETECTDLL_API int * facedetect_frontal_surveillance(unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
+	float scale, //scale factor for scan windows
+	int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
+	int min_object_width, //Minimum possible face size. Faces smaller than that are ignored.
+	int max_object_width = 0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
 
 #endif

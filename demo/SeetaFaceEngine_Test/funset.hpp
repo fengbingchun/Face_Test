41064534444e2fd45010581ddad0583d1a87d5b5
fengbingchun <fengbingchun@163.com>
2018-09-13 17:54:02 +0800

    Fix SeetaFaceEngine_Test bug

--- demo/SeetaFaceEngine_Test/funset.hpp
+++ demo/SeetaFaceEngine_Test/funset.hpp
@@ -8,7 +8,7 @@ int test_alignment();
 int test_recognize();
 int test_identification_CropFace();
 int test_identification_ExtractFeature();
-int test_identification_ExtractFeatureWithCrop();
+//int test_identification_ExtractFeatureWithCrop();
 std::string get_image_name(std::string name);
 
 #endif // FBC_FACE_TEST_SEETAFACEENGINE_HPP_
--- demo/Tadas_OpenFace_Test/Tadas_OpenFace_Test.cpp
+++ demo/Tadas_OpenFace_Test/Tadas_OpenFace_Test.cpp
@@ -5,6 +5,6 @@ int main()
 {
 	int ret = test_face_landmark();
 
-	if (ret == 0) fprintf(stderr, "test success\n");
-	else fprintf(stderr, "test fail\n");
+	if (ret == 0) fprintf(stdout, "====== test success ======\n");
+	else fprintf(stderr, "###### test fail ######\n");
 }
